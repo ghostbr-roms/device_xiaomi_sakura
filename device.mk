2df03ee4e15abdb23ac0f0006f14c147e9152b59
GiaSen <giammasena@gmail.com>
2019-09-10 12:15:29 +0600

    sakura: Nuke Lineage

--- device.mk
+++ device.mk
@@ -229,10 +229,6 @@ PRODUCT_COPY_FILES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.sakura
-	
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -361,10 +357,6 @@ PRODUCT_COPY_FILES += \
 # ThermalController app
 PRODUCT_PACKAGES += \
     ThermalController
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
